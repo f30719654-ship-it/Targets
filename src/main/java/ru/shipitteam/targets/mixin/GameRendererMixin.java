2b364da7a8298f757c95d5b017c94b6c91ae1e00
f30719654-ship-it <f30719654@gmail.com>
2026-06-06 14:11:29 +0300

    Update GameRendererMixin.java

--- src/main/java/ru/shipitteam/targets/mixin/GameRendererMixin.java
+++ src/main/java/ru/shipitteam/targets/mixin/GameRendererMixin.java
@@ -1,1 +1,9 @@
+package ru.shipitteam.targets.mixin;
 
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    // Пустой миксин, чтобы Fabric не выдавал ошибку при запуске
+}
