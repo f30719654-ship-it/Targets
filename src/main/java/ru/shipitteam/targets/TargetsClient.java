f0daa19186ad10657197db589d106c0a9ba6463c
f30719654-ship-it <f30719654@gmail.com>
2026-06-06 14:18:44 +0300

    Update TargetsClient.java

--- src/main/java/ru/shipitteam/targets/TargetsClient.java
+++ src/main/java/ru/shipitteam/targets/TargetsClient.java
@@ -1,1 +1,10 @@
+package ru.shipitteam.targets;
 
+import net.fabricmc.api.ClientModInitializer;
+
+public class TargetsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[Targets Mod] Мод успешно загружен!");
+    }
+}
